6404574a6ea256440df793541f727de141caaea7
usman <mirza@gmail.com>
2023-03-07 19:41:19 +0500

    I create the fagel

---
+++ scripts/script.js
@@ -0,0 +1,145 @@
+
+// Link 1
+const link1 = document.querySelector('#link1');
+const imageContainer = document.querySelector('#image-container');
+
+link1.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-11.jpg';
+});
+
+link1.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+
+// Link 2
+const link2 = document.querySelector('#link2');
+
+link2.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-2.jpg';
+});
+
+link2.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 3
+const link3 = document.querySelector('#link3');
+
+link3.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-9.jpg';
+});
+
+link3.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 4
+const link4 = document.querySelector('#link4');
+
+link4.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-06.jpg';
+});
+
+link4.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 5
+const link5 = document.querySelector('#link5');
+
+link5.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-14.jpg';
+});
+
+link5.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+// Link 6
+const link6 = document.querySelector('#link6');
+
+link6.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-4.jpg';
+});
+
+link6.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 7
+const link7 = document.querySelector('#link7');
+
+link7.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-5.jpg';
+});
+
+link7.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 8
+const link8 = document.querySelector('#link8');
+
+link8.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-8.jpg';
+});
+
+link8.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 9
+const link9 = document.querySelector('#link9');
+
+link9.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-1.jpg';
+});
+
+link9.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 10
+const link10 = document.querySelector('#link10');
+
+link10.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-012.jpg';
+});
+
+link10.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 11
+const link11 = document.querySelector('#link11');
+
+link11.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-03.jpg';
+});
+
+link11.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
+
+// Link 12
+const link12 = document.querySelector('#link12');
+
+link12.addEventListener('mouseover', () => {
+  imageContainer.classList.remove('hide');
+  imageContainer.querySelector('img').src = './images/h1-portfolio-img-010-1.jpg';
+});
+
+link12.addEventListener('mouseout', () => {
+  imageContainer.classList.add('hide');
+});
